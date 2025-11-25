de187dca4a9372361b0a58c8601d1b1741afbbea
Safvan <pcsajitha87@gmail.com>
2025-11-25 10:04:31 +0530

    Time: 2 ms (8.58%), Space: 53.6 MB (80.86%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function(a, b) {
+    let count = 0;
+    for(let i = 1; i<=Math.min(a,b);i++){
+        if(a % i ===0 && b % i === 0){
+          count++
+        }
+    }
+    return count
+};
+console.log(commonFactors(12,6))
