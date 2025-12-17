c96df35c416b6f04db3e90c537540cd398bee3c1
Safvan <pcsajitha87@gmail.com>
2025-12-17 13:42:14 +0530

    Time: 60 ms (45.57%), Space: 56.3 MB (43.52%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        let a = x;{
+            for(let i=functions.length-1;i>=0;i--){
+                a=functions[i](a);
+            }
+            return a;
+        }
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
