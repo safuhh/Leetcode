042e9c5bcbc535808a65ab7186892dbe97427e56
Safvan <pcsajitha87@gmail.com>
2026-01-03 10:53:22 +0530

    Time: 3 ms (89.4%), Space: 60.7 MB (53.11%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let res = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+    let total = 0;
+for(let i=0; i<s.length;i++){
+    let cur = res[s[i]]
+    const next = res[s[i+1]]
+    if(cur < next){
+    total+=(next - cur);
+    i++
+}else{
+    total +=cur;
+}
+}
+
+return total
+};
