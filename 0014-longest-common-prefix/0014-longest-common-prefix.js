7d45a237b0a7bc1282289650013ce16b4ce37bcd
Safvan <pcsajitha87@gmail.com>
2026-01-03 10:30:22 +0530

    Time: 0 ms (100%), Space: 53.9 MB (86.08%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length===0)return "";
+    let res=strs[0];
+    for(let i=1; i< strs.length;i++){
+        while(strs[i].indexOf(res)!==0){
+            res = res.slice(0,res.length-1);
+            if(res==="")return "";
+        }
+    }
+    return res;
+
+};
