4d1f858b65162a6221da48a714230eee8d819b8e
Safvan <pcsajitha87@gmail.com>
2026-01-09 10:56:10 +0530

    Time: 0 ms (100%), Space: 57.8 MB (56.45%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let low = 0;
+    let high = nums.length-1;
+  
+    while(low<=high){
+        const mid = Math.floor((low+high)/2);
+        if(nums[mid]===target){
+            return mid;
+            break;
+        }else if(target<nums[mid]){
+            high = mid -1
+        }else{
+            low=mid+1
+        }
+    }
+    return -1;
+};
