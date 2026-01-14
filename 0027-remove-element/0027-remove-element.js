dd70d20d928832c707da4d9fc4546a3ef5f497ff
Safvan <pcsajitha87@gmail.com>
2026-01-15 00:09:24 +0530

    Time: 0 ms (100%), Space: 54 MB (59.31%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,12 +4,9 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-      let i = 0;
-   while(i<nums.length){
-       if(nums[i]===val){
-           nums.splice(i,1)
-       }else{
-           i++
-       }
-   }
+for(let i=nums.length;i>=0;i--){
+    if(nums[i]===val){
+        nums.splice(i,1)
+    }
+}
 };
